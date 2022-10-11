8e12255da5fcb1060149621b2e041b3e26353ffd
Karen Noiva <knoiva@indecon.com>
2022-10-11 17:26:43 -0400

    Update createSystemData.R
    Updated `createSystemData()` to create `slrExtremes`: a list of SLR values, scaled impacts, slopes, and intercepts to be used for calculating SLR impacts above the defined values. Note: the update to "utils.R" should have mentioned a new function, `extend_slr()`, used to extend SLR values from the maximum defined year to 2300. The update to `createSystemData` makes use of these new functions.

--- createSystemData/R/createSystemData.R
+++ createSystemData/R/createSystemData.R
@@ -2,11 +2,11 @@
 ### 2021.02.10: Updated to process SLRs separately from other sectors
 ### The purpose of this function is to import data from Excel and use this data to create and save R data objects.
 createSystemData <- function(
-  projectPath = NULL, ### Path to project
-  excelName   = NULL, ### name of excel file with config information
-  outPath     = NULL,
-  save        = NULL,
-  silent      = NULL  ### Whether to message the user
+    projectPath = NULL, ### Path to project
+    excelName   = NULL, ### name of excel file with config information
+    outPath     = NULL,
+    save        = NULL,
+    silent      = NULL  ### Whether to message the user
 ){
   require(tidyverse)
   ###### Set up the environment ######
@@ -14,7 +14,7 @@ createSystemData <- function(
   silent  <- ifelse(is.null(silent), F, silent)
   msgUser <- !silent
   save    <- ifelse(is.null(save), F, save)
-
+  
   ###### Create File Paths ######
   projectPath <- ifelse(is.null(projectPath), ".", projectPath)
   ### Excel configuration file
@@ -54,154 +54,95 @@ createSystemData <- function(
     fileName  = extDataFile,
     sheetName = "tableNames",
     silent    = silent
-    )
-
+  )
+  
   if(msgUser){message("\t", messages_data[["loadInputs"]]$success)}
-
+  
   ### Assign data tables to objects in the list
   for(i in 1:length(loadDataList)){
     assign(names(loadDataList)[i], loadDataList[[i]])
   } ### End iterate over i
-
+  
   ###### Sector Info ######
   ### Exclude some sectors, get the number of sectors and sector info
   ### Sector info with additional sector info: df_sectorsInfo
   ### Sector info with models: df_sectorsModels
   num_sectors  <- co_sectors %>% nrow #; num_sectors
   sector_ids   <- co_sectors$sector_id
-
+  
   ###### Interpolate Default Scenario ######
   ### Interpolate annual values and calculate GDP per capita
   gdp_default <- co_defaultScenario %>% filter(region==co_regions$region[1]) %>%
     select(year, gdp_usd) %>% mutate(region="National.Total") %>%
     interpolate_annual(years= list_years, column = "gdp_usd", rule=1:2)
-
+  
   pop_default <- co_defaultScenario %>%
     select(year, reg_pop, region) %>%
     interpolate_annual(years= list_years, column="reg_pop", rule=1:2)
-
+  
   national_pop_default <- pop_default %>%
     group_by(year) %>%
     summarize_at(.vars=c("reg_pop"), sum, na.rm=T) %>%
     rename(national_pop = reg_pop) %>%
     mutate(region="National.Total")
-
+  
   # ### National scenario
   national_scenario_default <- gdp_default %>%
     left_join(national_pop_default, by=c("year", "region")) %>%
     mutate(gdp_percap = gdp_usd/national_pop)
-
+  
   ### Join GDP with national totals and population
   df_defaultScenario <- national_scenario_default %>%
     select(-region) %>%
     left_join(pop_default, by = "year")
   rm("national_scenario_default")
-
+  
   ###### Extreme SLR Scenarios ######
-  # dataList[["slr_cm"]]
-  # c_npdRefYear <- 2090
-  # c_maxSLRYear <- slr_cm$year %>% max
-  # df_slrYears  <- data.frame(year = (c_maxSLRYear + 1):maxYear, dummyCol = 1)
   ### Do nothing to slr_cm and slrImpacts
-  loadDataList[["slr_cm"    ]] <- slr_cm; rm("slr_cm")
-  loadDataList[["slrImpacts"]] <- slrImpacts; rm("slrImpacts")
+  # loadDataList[["slr_cm"    ]] <- slr_cm; #rm("slr_cm")
+  # loadDataList[["slrImpacts"]] <- slrImpacts; #rm("slrImpacts")
+  # slr_cm %>% names %>% print; slrImpacts %>% names %>% print; 
+  # slr_cm <- slr_cm %>% rename(model_type = model_type)
   ### Create data for extreme values above 250cm
   slrExtremes <- fun_slrConfigExtremes(
-      slr_x = rDataList_x$slr_cm, ### rDataList_x$slr_cm
-      imp_x = rDataList_x$slrImpacts ### rDataList_x$slrImpacts
-    )
-  loadDataList[["slrExtremes"]] <- slrExtremes; rm("slrExtremes")
+    slr_x = slr_cm, ### rDataList$slr_cm
+    imp_x = slrImpacts ### rDataList$slrImpacts
+  )
+  # loadDataList[["slrExtremes"]] <- slrExtremes; rm("slrExtremes")
   
   ###### Interpolate SLR Scenarios ######
-  # dataList[["slr_cm"]]
-  # c_npdRefYear <- 2090
-  c_maxSLRYear <- slr_cm$year %>% max
-  df_slrYears  <- data.frame(year = (c_maxSLRYear + 1):maxYear, dummyCol = 1)
-  ### SLR Heights
-  slr_cm       <- slr_cm %>% (function(x){
-      x2 <- x %>% 
-        filter(year==c_maxSLRYear) %>% 
-        mutate(dummyCol = 1) %>% 
-        select(-c("year")) %>%
-        left_join(df_slrYears, by = c("dummyCol")) %>%
-        select(-c("dummyCol"))
-      
-      x <- x %>% 
-        filter(year <= c_maxSLRYear) %>% rbind(x2) %>%
-        arrange_at(.vars = c("model", "year")) %>%
-        mutate(model_type = "slr")
-    })
-  loadDataList[["slr_cm"]] <- slr_cm; rm("slr_cm")
-  ### slrImpacts
-  slrImpacts <- slrImpacts %>% (function(x){
-      x2 <- x %>% 
-        filter(year==c_maxSLRYear) %>% 
-        mutate(dummyCol = 1) %>% select(-c("year")) %>%
-        left_join(df_slrYears, by = c("dummyCol")) %>%
-        select(-c("dummyCol"))
-      
-      x <- x %>% 
-        filter(year <= c_maxSLRYear) %>% rbind(x2) %>%
-        arrange_at(.vars = c("sector", "variant", "impactType", "impactYear", "region", "year"))
-    })
-  loadDataList[["slrImpacts"]] <- slrImpacts; rm("slrImpacts")
-  ### slrImpacts
-  slrExtremes <- slrExtremes %>% (function(x){
-    x2 <- x %>% 
-      filter(year==c_maxSLRYear) %>% 
-      mutate(dummyCol = 1) %>% select(-c("year")) %>%
-      left_join(df_slrYears, by = c("dummyCol")) %>%
-      select(-c("dummyCol"))
-    
-    x <- x %>% 
-      filter(year <= c_maxSLRYear) %>% rbind(x2) %>%
-      arrange_at(.vars = c("sector", "variant", "impactType", "impactYear", "region", "year"))
-  })
-  loadDataList[["slrExtremes"]] <- slrExtremes; rm("slrExtremes")
+  ### Extend SLR Heights, Impacts, and Extremes
+  c_cm         <- c("model", "year")
+  c_imp        <- c("sector", "variant", "impactType", "impactYear", "region", "year")
+  slr_cm       <- slr_cm      %>% extend_slr(arrange_x=c_cm)
+  slrImpacts   <- slrImpacts  %>% extend_slr(arrange_x=c_imp)
+  slrExtremes  <- slrExtremes %>% extend_slr(arrange_x=c_imp)
+  ### Update in data list and remove objects
+  loadDataList[["slr_cm"     ]] <- slr_cm; 
+  loadDataList[["slrImpacts" ]] <- slrImpacts
+  loadDataList[["slrExtremes"]] <- slrExtremes
+  rm("slr_cm", "slrImpacts", "slrExtremes"); rm("c_cm", "c_imp")
   
-    
+  
   ###### Format Scalar Tables ######
   ### Interpolate values to annual levels
-  scalarNameList <- scalarDataframe$scalarName %>% unique
-  numScalarNames <- scalarNameList %>%length
-  df_mainScalars <- 1:numScalarNames %>%
-    lapply(function(i){
-      name_i     <- scalarNameList[i]
-      which_i    <- which(co_scalarInfo$scalarName==name_i)
-      method_i   <- co_scalarInfo$constant_or_dynamic[which_i]
-      region_i   <- co_scalarInfo$national_or_regional[which_i]
-      # name_i %>% print
-      
-      ### Constant or linear
-      method_i   <- ifelse(method_i=="constant", method_i, "linear")
-      scalar_i   <- scalarDataframe %>% filter(scalarName==name_i) %>%
-        interpolate_annual(
-          years  = list_years,
-          column = "value",
-          rule   = 1:2,
-          method = method_i) %>%
-        mutate(scalarName=name_i)
-      return(scalar_i)
-    }) %>%
-    # (function(x){do.call(rbind, x)}) ; df_allScalars %>% glimpse
-    (function(x){do.call(rbind, x)}) %>%
-    ###### Add regional population and gdp_percap to scalars
-    select(c("scalarName", "region", "year", "value")) %>%
-    ### Bind the scalar info to the scalar data
-    left_join(co_scalarInfo %>% select(c("scalarName", "scalarType", "national_or_regional")), by=c("scalarName"))
+  # scalarDataframe %>% names %>% print
+  df_mainScalars    <- fun_formatScalars(
+    data_x  = scalarDataframe, ### rDataList$scalarDataframe
+    info_x  = co_scalarInfo, ### rDataList$co_scalarInfo
+    years_x = list_years ### rDataList$list_years
+  )
   
-  
-
   ###### Physical and Economic Scalars ######
   ### Physical scalars: Get population weights, then physical scalar multipliers
   ### Economic scalars: Get economic scalars, then multipliers
   if(msgUser){message(messages_data[["calcScalars"]]$try)}
-
-  ### Initialized results
-  df_results0 <- df_sectorsInfo %>% mutate(dummyCol=1) %>%
-    left_join(df_defaultScenario %>% mutate(dummyCol=1), by="dummyCol") %>%
-    select(-dummyCol)
-  rm("df_defaultScenario")
+  
+  ### Initialized results: Join sector info and default scenario
+  df_info0    <- df_sectorsInfo     %>% mutate(dummyCol=1)
+  df_default0 <- df_defaultScenario %>% mutate(dummyCol=1)
+  df_results0 <- df_info0 %>% left_join(df_default0, by="dummyCol") %>% select(-c("dummyCol"))
+  rm("df_sectorsInfo", "df_defaultScenario", "df_info0", "df_default0")
   ### Physical adjustment
   df_results0 <- df_results0  %>% match_scalarValues(df_mainScalars, scalarType="physAdj")
   ### Damage adjustment
@@ -212,62 +153,57 @@ createSystemData <- function(
   df_results0 <- df_results0 %>% select(-c("gdp_usd", "reg_pop", "national_pop", "gdp_percap"))
   ### Message the user
   if(msgUser){message("\t", messages_data[["calcScalars"]]$success)}
-
+  
   ###### Get Scenario Info for Scaled Impacts  ######
   ### Refactor adaptation, impact years, impact types
   ### Add a column with a scenario id
-  data_scaledImpacts <- data_scaledImpacts %>%
-    mutate(
-      scenario_id = paste(sector, variant, impactYear, impactType, model_type, model_dot, region_dot, sep="_")
-      )
-
+  data_scaledImpacts <- data_scaledImpacts %>% get_scenario_id(include=c("model_dot", "region_dot"))
+  
   ### Get list of scenarios for scenarios with at least some non-NA values
   impactScenariosList <- (
-    data_scaledImpacts %>%
+    data_scaledImpacts %>% 
       filter(!is.na(scaledImpact)) %>%
       as.data.frame
-    )$scenario_id %>% unique
-
+  )$scenario_id %>% unique
+  
   ### Add information on non-missing scenarios to scaled impacts data
-  data_scaledImpacts    <- data_scaledImpacts %>%
-    mutate(hasScenario = (scenario_id %in% impactScenariosList))
-
+  data_scaledImpacts    <- data_scaledImpacts %>% mutate(hasScenario = (scenario_id %in% impactScenariosList))
+  
   ###### Get Interpolation Functions for Scenarios ######
   ### Iterate over sectors to get interpolation functions with fun_getImpactFunctions()
   ### fun_getImpactFunctions depends on the function fun_tempImpactFunction()
   if(msgUser){message("\t", messages_data[["interpFuns"]]$try)}
-
   df_hasScenario         <- data_scaledImpacts %>% filter(hasScenario)
   list_impactFunctions   <- list()
-
+  
   # c_modelTypes   <- co_modelTypes$modelType_id
   c_modelTypes   <- c("gcm")
   for(modelType_i in c_modelTypes){
     ### Max output value, maximum extrapolation value, unit scale, extend type
-    df_model_i <- (co_modelTypes %>% filter(modelType_id==modelType_i))
-    maxOutput_i  <- df_model_i$modelMaxOutput %>% unique
-    maxExtrap_i  <- df_model_i$modelMaxExtrap %>% unique
-    unitScale_i  <- df_model_i$modelUnitScale %>% unique
-
+    df_model_i  <- (co_modelTypes %>% filter(modelType_id==modelType_i))
+    maxOutput_i <- df_model_i$modelMaxOutput %>% unique
+    maxExtrap_i <- df_model_i$modelMaxExtrap %>% unique
+    unitScale_i <- df_model_i$modelUnitScale %>% unique
+    ### Filter to data
     df_i        <- df_hasScenario %>% filter(model_type==modelType_i) %>% ungroup %>% as.data.frame
-
-    functions_i <- df_i  %>%
-      get_impactFunctions(
+    ### Get functions
+    functions_i <- df_i  %>% get_impactFunctions(
         groupCol    = "scenario_id",
         xCol        = "modelUnitValue",
         yCol        = "scaledImpact",
         extend_from = maxOutput_i,
         extend_to   = maxExtrap_i,
+        extend_all  = TRUE,
         unitScale   = unitScale_i
       ); # paste(modelType_i, length(functions_i)) %>% print
     ### Add values to list
     list_impactFunctions <- c(list_impactFunctions, functions_i)
   }
-
+  
   ###### Aggregate R Data objects ######
   ### Message the user
   if(msgUser){message("\t", messages_data[["interpFuns"]]$success)}
-
+  
   ### Update data list:
   loadDataList[["data_scaledImpacts"]] <- data_scaledImpacts
   ### Make a new data list
@@ -281,7 +217,7 @@ createSystemData <- function(
   )
   ### Combine with the data list
   rDataList <- c(loadDataList, rDataList)
-
+  
   ###### Save R Data objects ######
   ### If save:
   ### - Message the user
@@ -289,23 +225,23 @@ createSystemData <- function(
   ### - If the outpath exists, try to save the file
   if(save){
     message("\n", "Saving results to ", sysDataPath, "...")
-
+    
     outPathExists <- sysDataPath %>% dir.exists
-
+    
     if(outPathExists){
-
+      
       trySave <- try(
         save(fredi_config, rDataList, file=sysDataFile),
         silent=T
       )
     } ### End if outPathExists
   } ### End if save
-
+  
   # rDataList %>% names %>% print
-
+  
   ###### Return object ######
   message("\n\n", "Finished", ".")
-
+  
   return(rDataList)
 } ### End function
 
