e7968dd6a33fd27696477874aec1cb2df4e2510e
Karen Noiva <knoiva@indecon.com>
2024-02-23 10:06:45 -0500

    Updated documentation of datasets

--- FrEDI/R/FrEDI.R
+++ FrEDI/R/FrEDI.R
@@ -4,22 +4,26 @@
 #'
 #' [FrEDI] is an R package being developed by the U.S. Environmental Protection Agency (EPA). The functions and data provided by this package can be used to estimate climate change impacts for the contiguous United States (CONUS) using the Framework for Evaluating Damages and Impacts (FrEDI), developed as part of EPA's [Climate Change Impacts and Risk Analysis](https://epa.gov/cira/) (CIRA) project. [FrEDI] contains R code that implement FrEDI and allow users to project impacts from climate change and sea level rise for a selected set of sectors.
 #'
+#' For help getting started with [FrEDI], visit <https://usepa.github.io/FrEDI/articles/manual.html>.
+#'
+#' For additional package documentation, see <https://usepa.github.io/FrEDI/index.html>.
+#'
 #' For more information on the Framework and the CIRA project, visit <https://epa.gov/cira/>, especially <https://epa.gov/cira/FrEDI/>.
 #'
 #' @section Overview of Functions:
-#' The function [FrEDI::run_fredi()] provided in this package is the primary function implementing FrEDI The main inputs to [FrEDI::run_fredi()] are climate scenarios (temperature in degrees Celsius, global mean sea level rise in centimeters) and socioeconomic scenarios (gross domestic product, regional population). [FrEDI::run_fredi()] projects climate impacts for these scenarios using the temperature binning framework. Users can also specify the levels at which to aggregate the climate impacts.
+#' The function [FrEDI::run_fredi()] provided in this package is the primary function implementing `FrEDI` The main inputs to [FrEDI::run_fredi()] are climate scenarios (temperature in degrees Celsius, global mean sea level rise in centimeters) and socioeconomic scenarios (gross domestic product, regional population). [FrEDI::run_fredi()] projects climate impacts for these scenarios using the temperature binning framework. Users can also specify the levels at which to aggregate the climate impacts.
 #'
-#' [FrEDI] also contains functions to assist in the pre-processing of input scenarios and the post-processing of outputs. For instance, the pre-processing function [FrEDI::import_inputs()] helps users to import scenarios from user-specified comma-separated value (CSV) files, while the post-processing function [FrEDI::get_plots()] generates a couple of different types of images (creates heatmaps and ribbon plots of climate impacts over time) from the outputs of [FrEDI::run_fredi()].
+#' [FrEDI] also contains functions to assist in the pre-processing of input scenarios and the post-processing of outputs.
 #'
-#' Additional helper functions provided by [FrEDI] include [FrEDI::convertTemps()], [FrEDI::temps2slr()], [FrEDI::get_sectorInfo()], and [FrEDI::aggregate_impacts()].
+#' * Pre-processing functions include [FrEDI::get_sectorInfo()], [FrEDI::import_inputs()], [FrEDI::convertTemps()], [FrEDI::temps2slr()].
+#'      * [FrEDI::get_sectorInfo()] allows users to access a list of sectors within `FrEDI` and related sector information.
+#'      * [FrEDI::import_inputs()] helps users in importing custom scenarios from user-specified comma-separated value (CSV) files.
+#'      * [FrEDI::convertTemps()] helps users to convert between global mean temperature and temperatures for the contiguous United States (CONUS) (both in degrees Celsius).
+#'      * [FrEDI::temps2slr()] helps users to estimate global mean sea level rise (GMSL, in centimeters) from global mean temperature in degrees Celsius.
 #'
-#' [FrEDI::convertTemps()] helps users to convert between global mean temperature and temperatures for the contiguous United States (CONUS) (both in degrees Celsius). [FrEDI::temps2slr()] helps users to estimate global mean sea level rise (GMSL, in centimeters) from global mean temperature in degrees Celsius.
+#' [FrEDI::aggregate_impacts()] is a post-processing helper function that helps users to aggregate and summarize the outputs of [FrEDI]. [FrEDI::aggregate_impacts()] can be used to calculate national totals, model averages, sum over impact types, and interpolate between multiple impact years (note that [FrEDI::run_fredi()] will automatically run [FrEDI::aggregate_impacts()] before returning outputs if the `aggLevels` argument is not `"none"`).
 #'
-#' [FrEDI::get_sectorInfo()] allows users to access a list of sectors within FrEDI and related sector information.
-#'
-#' The post-processing helper function [FrEDI::aggregate_impacts()] helps users to aggregate and/or summarize the outputs of temperature binning (calculates national totals, model averages, impact totals, and interpolations between multiple impact years (for applicable sectors). Users have the option to run [FrEDI::aggregate_impacts()] on the outputs of [FrEDI::run_fredi()]. However, users can alternatively specify aggregation levels as arguments to the main function, [FrEDI::run_fredi()], which will run [FrEDI::aggregate_impacts()] for specified levels before returning outputs (by default, [FrEDI::run_fredi()] returns results aggregated to all aggregation levels).
-#'
-#' Version 2.3.0 includes a module, the FrEDI Social Vulnerability (SV) module for estimating impacts on socially vulnerable populations for select sectors. [FrEDI::get_sv_sectorInfo()] allows users to access a list of sectors within the FrEDI SV module and related sector information. The function [FrEDI::run_fredi_sv()] is the main function for the FrEDI SV module. [FrEDI::run_fredi_sv()] is designed to calculate impacts for a single sector at a time for a custom population scenario or one or more custom temperature or sea level rise scenarios. When running [FrEDI::run_fredi_sv()], users have the option to save results to an Excel file that provides basic data visualization for the module outputs. For more information on the data underlying the FrEDI SV module, visit <https://www.epa.gov/cira/social-vulnerability-report/>.
+#' Versions 2.3.0 and above include the `FrEDI` Social Vulnerability (SV) module for estimating impacts on socially vulnerable populations for select sectors. [FrEDI::get_sv_sectorInfo()] allows users to access a list of sectors within the FrEDI SV module and related sector information. The function [FrEDI::run_fredi_sv()] is the main function for the `FrEDI` SV module. [FrEDI::run_fredi_sv()] is designed to calculate impacts for a single sector at a time for a custom population scenario or one or more custom temperature or sea level rise scenarios. When running [FrEDI::run_fredi_sv()], users have the option to save results to an Excel file that provides basic data visualization for the module outputs. For more information on the data underlying the `FrEDI` SV module, visit <https://www.epa.gov/cira/social-vulnerability-report/>.
 #'
 #'  @section Overview of Package Contents:
 #' [FrEDI] consists of files in the following directories:
@@ -27,20 +31,23 @@
 #' * __data__. Contains R Data files ending in `".rdb"`, `".rds"`, and `".rdx"`, containing data included with the package.
 #' * __help__ and __html__. Contain documentation for functions available to the user, including function descriptions, lists of arguments and outputs, and examples. See `"html/00Index.html"` or the individual R help functions for more information about individual functions.
 #' * __Meta__. Contains RDS files (ending in `".rds"`) with information about the package contents.
-#' * __extdata__. __extdata/scenarios__ Contains three CSV files for users to test the function for importing data. (`"pop_scenario.csv"`, `"slr_from_GCAM.csv"`, and `"GCAM_scenario.csv"`) respectively contain examples of a population scenario, sea level rise scenario, and temperature scenario. For more information, refer to documentation for the function `"import_inputs()"`. __extdata/sv__ contains files used by the FrEDI SV module.
+#' * __extdata__. __extdata/scenarios__ Contains four CSV files for users to test the function for importing data. For more information on importing scenarios for use with [FrEDI::run_fredi()], refer to documentation for the function [FrEDI::import_inputs()].
+#'      * `"GCAM_scenario.csv"` contains a set of temperature scenarios that can be used with [FrEDI], including the default temperature scenario used by both [FrEDI::run_fredi()] and [FrEDI::run_fredi_sv()] (labeled `"description="Hector_GCAM_v5.3_ECS_3.0_REF"` in `"slr_from_GCAM.csv"` and `"ECS_3.0_ref_0"` in the [FrEDI::gcamScenarios] dataset)). Also see the documentation for the [FrEDI::gcamScenarios] dataset for more information.
+#'      * `"State ICLUS Population.csv"` and `"Region ICLUS Population.csv"` contain the default population scenarios used by [FrEDI::run_fredi()] and [FrEDI::run_fredi_sv()], respectively (see [FrEDI::popScenario] and [FrEDI::popScenario_sv]).
+#'      * `"slr_from_GCAM.csv"` contains global mean sea level rise heights in centimeters (created from the reference temperature scenario).
+#'  * __extdata/sv__ Contains files used by the `FrEDI` SV module to calculate impacts.
 #'
-#' The FrEDI R package contains a dataset with default results `defaultResults`, which contains annual impacts produced by [FrEDI::run_fredi()] for the default scenarios (i.e., default temperature, GDP and regional population trajectories), and can be loaded into the R workspace (`data(defaultResults))`. rEDI R package also provides a set of driver scenarios (`gcamScenarios`) and a population scenario (`popScenario`) for use with the FrEDI SV Module (which can be loaded into the workspace using the `data()` function).
+#' The `FrEDI` package contains a loadable dataset with default results `defaultResults`, which contains annual impacts produced by [FrEDI::run_fredi()] for the with the default options and default default scenarios (i.e., default temperature, GDP and regional population trajectories). Other loadable datasets provided by [FrEDI] are a set of driver scenarios ([FrEDI::gcamScenarios]), a state-level population scenario ([FrEDI::popScenario]) for use with [FrEDI::run_fredi()], and a region-level `popScenario_sv` for use with [FrEDI::run_fredi_sv()]. These data can be loaded into the workspace using the `data()` function.
 #'
 #' Typical use will involve `library(FrEDI)` or `require(FrEDI)`.
 #'
 #'
 #' @section Status:
-#' All code in this repository is being provided in a "draft" state and has not been reviewed or cleared by U.S. EPA. This status will be updated as models are reviewed.
+#' Disclaimer: All code in this repository is being provided in a "draft" state and has not been reviewed or cleared by U.S. EPA. This status will be updated as models are reviewed.
 #'
 #' @section Dependencies:
-#' [FrEDI] requires R (>= 3.5.0).
+#' [FrEDI] requires R (>= 4.2.0).
 #'
-# Installing [FrEDI] requires [devtools] (Tools to Make Developing R Packages Easier). More information on [devtools] can be found [here.](https://cran.r-project.org/web/packages/devtools/index.html) [devtools] can be installed using `install.packages("devtools")`, or see [link](https://www.r-project.org/nosvn/pandoc/devtools.html) for more information.
 #'
 #' [FrEDI] depends on:
 #' * [tidyverse] (Easily Install and Load the 'Tidyverse'). The official documentation for [tidyverse] can be found [here](https://cran.r-project.org/web/packages/tidyverse/index.html). [tidyverse] can be installed using `install.packages("tidyverse")`, or see [link](https://tidyverse.tidyverse.org/) for more information.
@@ -55,8 +62,7 @@
 #'
 #' By submitting a pull request to the GitHub and/or by installing this package, you make an agreement with EPA that you will not submit a claim of compensation for services rendered to EPA or any other federal agency. Further, you agree not to charge the time you spend developing software code related to this project to any federal grant or cooperative agreement.
 #'
-#'
-# @docType package
+#' @keywords internal
 "_PACKAGE"
 #' @name FrEDI
 #' @md
