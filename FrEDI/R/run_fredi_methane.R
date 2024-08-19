25f680bdb42ac4de4ba2aef4c6726b98b9109b68
Karen Noiva <knoiva@indecon.com>
2024-08-19 19:03:17 -0400

    Initialized methane function
    Got methane function working and added methane utilities

--- FrEDI/R/run_fredi_methane.R
+++ FrEDI/R/run_fredi_methane.R
@@ -18,26 +18,28 @@ run_fredi_methane <- function(
     allCols    = FALSE, ### Whether to include additional columns in output
     silent     = TRUE   ### Whether to message the user
 ){
-  ###### Load Objects ######
+  ###### Set up the environment ######
+  ###### ** Messaging ######
+  ### Level of messaging (default is to message the user)
+  msgUser   <- !silent
+
+
+  ###### ** Load Data ######
   ### Assign data objects to objects in this namespace
   ### Assign FrEDI config
   fredi_config <- rDataList[["fredi_config"]]
   for(name_i in fredi_config |> names()) {name_i |> assign(fredi_config[[name_i]]); rm(name_i)}
 
-  calc_NOx_factor <- listMethane$package$coefficients$NOx      [["fun0"]]
-  calc_mortality  <- listMethane$package$coefficients$Mortality[["fun0"]]
-  NOxFactor0      <- listMethane$package$coefficients$NOx$NOxFactor0
+  ### Coefficients
+  minYear0   <- listMethane[["package"]][["coefficients"]][["minYear0"]]
+  maxYear0   <- listMethane[["package"]][["coefficients"]][["maxYear0"]]
 
-  ###### Set up the environment ######
-  ### Level of messaging (default is to message the user)
-  msgUser   <- !silent
-  ### Uncomment for allCols
-  # doPrimary <- F  ### whether to filter to primary impacts
   ### Model years and NPD (FrEDI past 2100)
-  minYear   <- listMethane[["package"]][["coefficients"]][["minYear0"]]
+  minYear   <- minYear0
   maxYear   <- thru2300 |> ifelse(npdYear0, maxYear)
   do_npd    <- maxYear > maxYear0
-  # "got here2" |> print()
+
+
 
   ###### ** Return List ######
   ### Initialize list to return
@@ -86,15 +88,16 @@ run_fredi_methane <- function(
   } ### End if
   rm(has_elasticity, elasticity0)
 
+
+
   ###### ** State Info ######
   byState        <- TRUE
-  popCol0        <- "pop"
+  popCol0        <- c("pop")
   stateCols0     <- c("state", "postal")
 
 
 
-
-  ###### Scenarios ######
+  ###### Input Scenarios ######
   ###### ** Input Info ######
   paste0("Checking scenarios...") |> message()
   ### Add info to data
@@ -116,37 +119,21 @@ run_fredi_methane <- function(
   ### Get list with expected name of columns used for unique ids
   ### Get list with expected name of column containing values
   valCols0     <- co_inputInfo |> pull(valueCol) |> as.list() |> set_names(inNames0)
-  idCols0      <- list(valCols0=valCols0, df0=inputDefs[inNames0]) |> pmap(function(valCols0, df0){
-    df0 |> names() |> get_matches(y=valCols0, matches=F)
-  }) |> set_names(inNames0)
+  idCols0      <- inNames0     |> map(get_import_inputs_idCols) |> set_names(inNames0)
+  # idCols0      <- list(valCols0=valCols0, df0=inputDefs[inNames0]) |> pmap(function(valCols0, df0){
+  #   df0 |> names() |> get_matches(y=valCols0, matches=F)
+  # }) |> set_names(inNames0)
 
 
 
   ###### ** Input Defaults ######
-  # listMethane$package$ch4_default <- listMethane$package$ch4_default |> rename(CH4_ppbv = delta_ch4)
-  # listMethane$package$nox_default <- listMethane$package$nox_default |> rename(NOx_Mt   = NOx      )
-  # listMethane$package$o3_response <- listMethane$package$o3_default
-  # listMethane$package$o3_default  <- listMethane$package$ch4_default |> left_join(
-  #   "nox_default" |> get_frediDataObj(listSub="package", listName="listMethane"),
-  #   by=c("year")
-  # ) |>
-  #   mutate(O3_pptv_per_ppbv = NOx_Mt |> calc_NOx_factor()) |>
-  #   mutate(O3_pptv_per_ppbv = O3_pptv_per_ppbv / NOxFactor0) |>
-  #   mutate(O3_pptv_per_ppbv = O3_pptv_per_ppbv * CH4_ppbv) |>
-  #   select(c("year", "O3_pptv_per_ppbv"))
-
   inputDefs    <- inNames0 |> map(function(name0){
     ### Get defaults
     defName0 <- name0    |> paste0("_default")
-    df0      <- defName0 |> get_frediDataObj(listSub="package", listName="listMethane")
+    df0      <- defName0 |> get_frediDataObj(listSub="scenarioData", listName="listMethane")
     ### Format defaults
-    do_ch4_0 <- "ch4" %in% name0
-    do_nox_0 <- "nox" %in% name0
     do_o3_0  <- "o3"  %in% name0
-    if(do_ch4_0) df0 <- df0 |> rename(CH4_ppbv = delta_ch4)
-    if(do_nox_0) df0 <- df0 |> rename(NOx_Mt   = NOx      )
     if(do_o3_0 ) {
-      df0 <- df0 |> rename(O3_pptv_per_ppbv = o3_response)
       df0 <- df0 |> select(c(idCols0[["o3"]], valCols0[["o3"]]))
     } ### End if(do_o3_0 )
     ### Return
@@ -178,8 +165,9 @@ run_fredi_methane <- function(
 
   ### Need scenario for CH4 & NOX or O3:
   ### If has O3, use O3. Otherwise, use CH4
-  has_o3     <- inputsList[["o3" ]] |> nrow()
-  has_ch4    <- inputsList[["ch4"]] |> nrow()
+  has_o3     <- !(inputsList[["o3" ]] |> is.null())
+  has_o3 |> print()
+  # has_ch4    <- !(inputsList[["ch4"]] |> is.null())
   if(has_o3) {
     inputsList <- inputsList |> (function(list0, y=c("ch4", "nox")){list0[!((list0 |> names() %in% y))]})()
     inputDefs  <- inputDefs  |> (function(list0, y=c("ch4", "nox")){list0[!((list0 |> names() %in% y))]})()
@@ -295,175 +283,78 @@ run_fredi_methane <- function(
 
 
 
-  ###### ** Physical Driver Scenario  ######
+  ###### Physical Driver Scenario  ######
   ### Need scenario for CH4 & NOX or O3
-  has_ch4    <- inputsList[["ch4"]] |> nrow()
-  has_nox    <- inputsList[["nox"]] |> nrow()
-  has_o3     <- inputsList[["o3" ]] |> nrow()
+  has_ch4    <- inputsList[["ch4"]] |> nrow() |> length()
+  has_nox    <- inputsList[["nox"]] |> nrow() |> length()
+  has_o3     <- inputsList[["o3" ]] |> nrow() |> length()
   has_driver <- (has_ch4 & has_nox) | has_o3
-  if(!has_driver) {
-    1 |> get_msgPrefix(newline=T) |> paste0("Warning! `run_fredi_methane()` requires :") |> message()
-  }
-  select0    <- c("inputName")
-  filter0    <- c("temp", "slr")
-  df_drivers <- inputsList[filter0] |> combine_driverScenarios(info0 = df_inputInfo)
-  df_drivers <- df_drivers |> filter(year >= minYear, year <= maxYear)
+  if(has_o3) {
+    df_drivers <- inputsList[["o3"]]
+  } else{
+    join0      <- c("year")
+    df_drivers <- inputsList[["ch4"]] |> left_join(inputsList[["nox"]], by=join0)
+    rm(join0)
+  } ### End if(has_o3)
+
+  ### Get RR scalar and ozone response data
+  df_drivers <- df_drivers |> (function(df0, df1=listMethane$package$state_rrScalar){
+    ### Join data
+    join0 <- df0 |> names() |> get_matches(y=df1 |> names())
+    df0   <- df0 |> left_join(df1, by=join0, relationship="many-to-many")
+    rm(df1)
+    ### Calculate O3 if methane and nox present
+    do_o3 <- df0 |> names() |> get_matches(y=c("CH4_ppbv")) |> length()
+    if(do_o3) df0 <- df0 |> calc_o3_conc()
+  })()
   # return(df_drivers)
 
-  ###### ** Socioeconomic Driver Scenario ######
+  ###### Socioeconomic Driver Scenario ######
   ### Update values
   gdp_df       <- inputsList[["gdp"]]
   pop_df       <- inputsList[["pop"]]
   pop_df       <- pop_df |> mutate(region = region |> str_replace(" ", ""))
   pop_df       <- pop_df |> mutate(region = region |> str_replace("\\.", ""))
-  ### ### Subset to desired range
-  pop_df       <- pop_df |> filter(year >= minYear, year <= maxYear)
-  gdp_df       <- gdp_df |> filter(year >= minYear, year <= maxYear)
   # return(pop_df)
   ### Calculate national population and update national scenario
-  seScenario   <- gdp_df |> create_nationalScenario(pop0 = pop_df)
+  natScenario  <- gdp_df |> create_nationalScenario(pop0 = pop_df)
+  rm(gdp_df, pop_df)
 
+  ### Calculate for CONUS values
+  seScenario   <- natScenario |> calc_conus_scenario()
   # return(seScenario)
   # seScenario |> pull(region) |> unique() |> print()
-  rm(gdp_df, pop_df)
   # seScenario |> glimpse()
 
   ###### Calculate Impacts ######
-  ###### ** Initialize Impacts Data frame ######
+  ###### ** Calculate Scalars ######
   ### Initialized results: Join sector info and default scenario
   ### Calculate physical scalars and economic multipliers then calculate scalars
   paste0("Calculating impacts...") |> message()
-  df_results   <- seScenario |> initialize_resultsDf(sectors=sectorIds, elasticity=elasticity)
+  df_results   <- seScenario |> calc_methane_scalars(elasticity = elasticity)
   # return(df_results)
   # df_results |> select(c("year", "gdp_usd", "national_pop", "gdp_percap")) |> unique() |> nrow() |> print()
-  # return()
-  # df_results |> pull(region) |> unique() |> print()
-  # df_results |> glimpse()
-  # return(df_results)
 
-  ### Get scaled impacts
-  df_impacts   <- sectorIds |> calc_scaled_impacts_fredi(drivers0 = df_drivers)
-  # df_impacts   <- df_results |> calc_scaled_impacts_fredi(drivers0 = df_drivers)
-  # df_impacts |> glimpse()
-  # return(df_impacts)
-  # return(list(df0=df_results, df1=df_impacts))
+  ###### ** Calculate Mortality Rate ######
+  df_results <- df_results |> calc_mortality()
 
-  ### Get impacts
-  df_results   <- df_results |> calc_impacts_fredi(df1=df_impacts)
-  # df_results |> pull(region) |> unique() |> print()
-  # df_results   <- df_impacts |> calc_impacts_fredi()
-  # return(list(df0=df_results, df1=df_impacts))
-  # rm(df_impacts)
-  # df_results |> glimpse()
-  # df_results |> pull(region) |> unique() |> print()
-  # return(df_results)
+  ###### ** Calculate Excess Mortality ######
+  df_results <- df_results |> (function(df0, df1=df_drivers){
+    ### Join data with drivers
+    join0 <- df0 |> names() |> get_matches(y=df1 |> names())
+    df0   <- df0 |> left_join(df1, by=join0)
+    rm(df1)
+    ### Calculate excess mortality
+    df0   <- df0 |> mutate(physical_impacts = pop * respMrate * state_mortScalar * O3_pptv)
+    ### Calculate annual impacts
+    df0   <- df0 |> mutate(annual_impacts   = physical_impacts * econScalar)
+  })()
 
 
   ###### Format Results ######
   ### Add in model info
   paste0("Formatting results", "...") |> message()
 
-  ###### ** Get Labels ######
-  ### Rename sector
-  drop0       <- c("sector", "variant", "impactType", "impactYear", "region", "modelType", "model")
-  renameAt0   <- drop0 |> paste0("_label") |> c("modelUnitDesc", "modelUnit_label", "modelUnitValue")
-  renameTo0   <- drop0 |> c("driverType", "driverUnit", "driverValue")
-  df_results  <- df_results |> select(-any_of(drop0))
-  df_results  <- df_results |> rename_at(c(renameAt0), ~renameTo0)
-  rm(drop0, renameAt0, renameTo0)
-  # df_results |> glimpse()
-
-  ### Rename model type
-  renameAt0   <- c("modelType")
-  renameTo0   <- c("model_type")
-  df_results  <- df_results |> rename_at(c(renameAt0), ~renameTo0)
-  rm(renameAt0, renameTo0)
-  # df_results |> pull(region) |> unique() |> print()
-  # return(df_results)
-
-  ###### ** Columns List ######
-  ### Grouping columns
-  groupCols0  <- c("sector", "variant", "impactType", "impactYear", "region") |> c(stateCols0)
-  groupCols0  <- groupCols0 |> c("model_type", "model")
-  groupCols0  <- groupCols0 |> c("sectorprimary", "includeaggregate")
-  groupCols0  <- groupCols0 |> c("physicalmeasure")
-  groupCols0  <- groupCols0 |> get_matches(y=df_results |> names(), matches=TRUE)
-  groupCols0  <- groupCols0 |> unique()
-  # groupCols0 |> print()
-  ### Driver columns
-  driverCols0 <- c("driverType", "driverUnit", "driverValue")
-  ### National & regional scenario columns
-  scenCols0   <- c("gdp_usd", "national_pop", "gdp_percap") |> c(popCol0)
-  ### Impact columns
-  impactCols0 <- c("physical_impacts", "annual_impacts")
-  ### Columns to select
-  select0     <- groupCols0 |> c(driverCols0, scenCols0) |> c("year")
-  ### Relocate columns
-  # df_results |> glimpse()
-  df_results  <- df_results |> relocate(all_of(select0))
-  # df_results  <- df_results |> relocate(any_of(select0))
-
-  ### Scalar columns
-  scalarCols0 <- c("physScalar", "physAdj", "damageAdj", "econScalar", "econAdj", "econMultiplier")
-  infoCols0   <- c("c0", "c1", "exp0", "year0")
-  suffix0     <- c("Name", "Value")
-  scalarCols0 <- scalarCols0 |> map(~.x |> paste0(c(suffix0))) |> unlist()
-  scalarCols0 <- scalarCols0 |> c("physScalar", "econScalar", "physEconScalar")
-  scalarCols0 <- scalarCols0 |> c("scaled_impacts")
-  scalarCols0 <- infoCols0   |> c(scalarCols0)
-  rm(suffix0, infoCols0)
-  ### Rearrange or drop scalar columns
-  if(allCols){
-    df_results  <- df_results |> relocate(any_of(scalarCols0), .after=all_of(select0))
-  } else{
-    df_results  <- df_results |> select(-any_of(scalarCols0))
-  } ### End if(allCols)
-
-  ### Other columns
-  # excludeCols
-  # otherCols0  <- df_results |> names() |> (function(x){x[!(x %in% c(select0, scalarCols0, impactCols0))]})()
-  otherCols0  <- df_results |> names() |> get_matches(y=c(select0, scalarCols0, impactCols0), matches=FALSE)
-  df_results  <- df_results |> select(-all_of(otherCols0))
-
-  ### Convert to character and drop sector id
-  df_results  <- df_results |> mutate_at(c(groupCols0), as.character)
-
-  ###### ** Primary Columns ######
-  mutate0     <- c("sectorprimary", "includeaggregate")
-  df_results  <- df_results |> mutate_at(c(mutate0), as.numeric)
-  rm(mutate0)
-  # df_results |> glimpse()
-  # df_results |> pull(region) |> unique() |> print()
-  # return(df_results)
-
-
-  ###### ** Aggregation ######
-  ### For regular use (i.e., not impactYears), simplify the data: groupCols0
-  if(doAgg) {
-    # doAgg |> print()
-    group0     <- groupCols0
-    df_results <- df_results |> aggregate_impacts(
-      aggLevels   = aggLevels,
-      groupByCols = group0,
-      columns     = impactCols0
-    ) ### End aggregate_impacts
-  } ### End if(doAgg)
-
-  ###### Arrange Outputs ######
-  ### Convert levels to character
-  ### Order the rows, then order the columns
-  # arrange0   <- groupCols0 |> c("year")
-  arrange0   <- groupCols0 |> c("year") |> get_matches(y = df_results |> names())
-  # arrange0 |> print()
-  ### Select columns
-  df_results <- df_results |> arrange_at(c(arrange0))
-  df_results <- df_results |> relocate(any_of(select0))
-  rm(arrange0)
-
-  ###### Format as Tibble ######
-  ### Update results in list
-  df_results   <- df_results |> as_tibble()
-  df_results   <- df_results |> ungroup()
 
   ###### Return Object ######
   ### Which object to return
